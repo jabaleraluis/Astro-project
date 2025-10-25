a32c76fd64b7e4dfe2447ee6148b5809d7bffe6b
Luis Jabalera <wichojabalera@gmail.com>
2025-10-24 18:00:03 -0700

    scrollreveal

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,48 @@
 import themeSwitcher from "./theme_switcher.js";
 import navbarMenuToggle from "./navbar_menu_toggle.js";
-/* import glowHover from "./glow.js"; */
+import glowEffect from "./glow_effect.js";
+import scrollUp from "./scroll_up.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   themeSwitcher(".theme__switcher .theme__icon");
-  navbarMenuToggle(".navbar__container .navbar__toggle__btn", ".navbar__container .navbar__links", ".navbar__container .navbar__links a");
-  /* glowHover(); */
+  navbarMenuToggle(
+    ".navbar__container .navbar__toggle__btn",
+    ".navbar__container .navbar__links",
+    ".navbar__container .navbar__links a"
+  );
+  scrollUp(".scroll__up");
+  glowEffect(".slide-track .slide .tech-item");
+
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    easing: "ease",
+    duration: 800,
+    delay: 150,
+    /* reset: true, */
+    once: true,
+    cleanup: true,
+  });
+
+  sr.reveal(
+    `.presentation__title h1, .presentation__title .name__line, .presentation__title h2,
+    .tech__preview .title, .tech__preview .see__all`,
+    {
+      origin: "left",
+      interval: 200,
+    }
+  );
+  sr.reveal(".presentation__buttons, .header__picture .picture", {
+    origin: "bottom",
+    interval: 200,
+  });
+  sr.reveal(".tech__preview .slider", { origin: "right" });
+  /* sr.reveal(".tech__wrapper .tech-item", {
+    origin: "right",
+    interval: 50,
+  }); */
+  sr.reveal(".header__picture .arrows, .header__picture .background", {
+    interval: 200,
+    scale: .75
+  });
 });
