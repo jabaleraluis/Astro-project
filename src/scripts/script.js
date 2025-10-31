9be2b64e4c77dd8d1267d12967864cd87575a95a
Luis Jabalera <wichojabalera@gmail.com>
2025-10-31 16:42:31 -0700

    add recent works grid

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -13,20 +13,23 @@ document.addEventListener("DOMContentLoaded", (e) => {
   scrollUp(".scroll__up");
   glowEffect(".slide-track .slide .tech-item");
 
+  /* ===== SCROLL REVEAL ===== */
+  /* const sr = {
   const sr = ScrollReveal({
     origin: "top",
     distance: "30px",
     easing: "ease",
     duration: 800,
     delay: 150,
-    /* reset: true, */
+    reset: true,
     once: true,
     cleanup: true,
   });
+  sr.reveal("header");
 
   sr.reveal(
     `.presentation__title h1, .presentation__title .name__line, .presentation__title h2,
-    .tech__preview .title, .tech__preview .see__all`,
+      .tech__preview .title, .tech__preview .see__all`,
     {
       origin: "left",
       interval: 200,
@@ -37,12 +40,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
     interval: 200,
   });
   sr.reveal(".tech__preview .slider", { origin: "right" });
-  /* sr.reveal(".tech__wrapper .tech-item", {
-    origin: "right",
-    interval: 50,
-  }); */
+
   sr.reveal(".header__picture .arrows, .header__picture .background", {
     interval: 200,
-    scale: .75
+    scale: 0.75,
   });
+  }; */
+
+
+
 });
