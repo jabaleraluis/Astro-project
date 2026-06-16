ec6bf6ccf768a4c45df92f7edb83a240534a859f
Luis Jabalera <jabaleraluisangel@gmail.com>
2026-06-16 12:00:03 -0700

    Update dependencies, enhance tech stack section with reveal animations, and improve code formatting

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,52 +1,17 @@
-import themeSwitcher from "./theme_switcher.js";
-import navbarMenuToggle from "./navbar_menu_toggle.js";
-import glowEffect from "./glow_effect.js";
-import scrollUp from "./scroll_up.js";
+import themeSwitcher from './theme_switcher.js';
+import navbarMenuToggle from './navbar_menu_toggle.js';
+import glowEffect from './glow_effect.js';
+import scrollUp from './scroll_up.js';
+import scrollReveal from './scroll_reveal.js';
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  themeSwitcher(".theme__switcher .theme__icon");
+document.addEventListener('DOMContentLoaded', (e) => {
+  themeSwitcher('.theme__switcher .theme__icon');
   navbarMenuToggle(
-    ".navbar__container .navbar__toggle__btn",
-    ".navbar__container .navbar__links",
-    ".navbar__container .navbar__links a"
+    '.navbar__container .navbar__toggle__btn',
+    '.navbar__container .navbar__links',
+    '.navbar__container .navbar__links a',
   );
-  scrollUp(".scroll__up");
-  glowEffect(".slide-track .slide .tech-item");
-
-  /* ===== SCROLL REVEAL ===== */
-  // const sr = {
-  const sr = ScrollReveal({
-    origin: "top",
-    distance: "30px",
-    easing: "ease",
-    duration: 800,
-    delay: 150,
-    reset: true,
-    once: true,
-    cleanup: true,
-  });
-  sr.reveal("header");
-
-  sr.reveal(
-    `.presentation__title h1, .presentation__title .name__line, .presentation__title h2,
-      .tech__preview .title, .tech__preview .see__all`,
-    {
-      origin: "left",
-      interval: 200,
-    }
-  );
-  sr.reveal(".presentation__buttons, .header__picture .picture", {
-    origin: "bottom",
-    interval: 200,
-  });
-  sr.reveal(".tech__preview .slider", { origin: "right" });
-
-  sr.reveal(".header__picture .arrows, .header__picture .background", {
-    interval: 200,
-    scale: 0.75,
-  });
-  // };
-
-
-
+  scrollUp('.scroll__up');
+  glowEffect('.slide-track .slide .tech-item');
+  scrollReveal();
 });
